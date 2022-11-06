684ba01f64075c9a79b840044860dee34958615c
sinpaiiii <mohammednowfal7228@gmail.com>
2022-11-06 13:49:57 +0530

    Updates

---
+++ precache-manifest.959e1786ce9b9589658cdcf6debbc2f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "238e29bc207ee2e37dfa",
+    "url": "/sinpaiiii/digiplus/static/css/main.7dc1577e.chunk.css"
+  },
+  {
+    "revision": "238e29bc207ee2e37dfa",
+    "url": "/sinpaiiii/digiplus/static/js/main.238e29bc.chunk.js"
+  },
+  {
+    "revision": "19dccbb455d8b9432847",
+    "url": "/sinpaiiii/digiplus/static/css/1.39f49839.chunk.css"
+  },
+  {
+    "revision": "19dccbb455d8b9432847",
+    "url": "/sinpaiiii/digiplus/static/js/1.19dccbb4.chunk.js"
+  },
+  {
+    "revision": "cf5b997bc9da9faf2149",
+    "url": "/sinpaiiii/digiplus/static/js/runtime~main.cf5b997b.js"
+  },
+  {
+    "revision": "b2027919e4302a32c61d3b2d21fa7349",
+    "url": "/sinpaiiii/digiplus/index.html"
+  }
+];
